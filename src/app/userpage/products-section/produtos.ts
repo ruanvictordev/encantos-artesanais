d30e0935a38a9945ff20067cba5487088a537a50
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2024-07-02 18:57:03 -0300

    :art: [STYLE] Create a Product Style.

--- src/app/userpage/products-section/produtos.ts
+++ src/app/userpage/products-section/produtos.ts
@@ -13,9 +13,9 @@ export interface IProdutoCarrinho extends IProduto {
 }
 
 export const produtos: IProduto[] = [
-    { id: 1, nome: "Boneca de pano", tamanho: "30cm", tecido: "Algodão", descricao: "Boneca bonita", preco: 39.00, imagem: "./assets/mouse-3.jpg" },
-    { id: 2, nome: "Chaveiro de Boneca", tamanho: "30cm", tecido: "Algodão", descricao: "Boneca bonita", preco: 39.00, imagem: "./assets/mouse-3.jpg" },
-    { id: 3, nome: "Bolsa de boneca", tamanho: "30cm", tecido: "Algodão", descricao: "Boneca bonita", preco: 39.00, imagem: "./assets/mouse-3.jpg" },
-    { id: 4, nome: "Bebê boneca", tamanho: "30cm", tecido: "Algodão", descricao: "Boneca bonita", preco: 39.00, imagem: "./assets/mouse-3.jpg" },
-    { id: 5, nome: "Luffy", tamanho: "30cm", tecido: "Algodão", descricao: "Boneca bonita", preco: 39.00, imagem: "./assets/mouse-3.jpg" },
+    { id: 1, nome: "Boneca de pano", tamanho: "30cm", tecido: "Algodão", descricao: "Boneca bonita", preco: 39.00, imagem: "assets/images/products/p1.png" },
+    { id: 2, nome: "Chaveiro de Boneca", tamanho: "30cm", tecido: "Algodão", descricao: "Boneca bonita", preco: 39.00, imagem: "assets/images/products/p2.png" },
+    { id: 3, nome: "Bolsa de boneca", tamanho: "30cm", tecido: "Algodão", descricao: "Boneca bonita", preco: 39.00, imagem: "assets/images/products/p3.png" },
+    { id: 4, nome: "Bebê boneca", tamanho: "30cm", tecido: "Algodão", descricao: "Boneca bonita", preco: 39.00, imagem: "assets/images/products/p4.png" },
+    { id: 5, nome: "Luffy", tamanho: "30cm", tecido: "Algodão", descricao: "Boneca bonita", preco: 39.00, imagem: "assets/images/products/p4.png" },
 ]
