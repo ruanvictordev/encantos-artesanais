7a87d3efdf73484641a837a91f795615f3155466
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2024-07-09 16:42:37 -0300

    :sparkles: [FEAT] New Products.

--- src/app/userpage/products-section/produtos.ts
+++ src/app/userpage/products-section/produtos.ts
@@ -13,11 +13,11 @@ export interface IProdutoCarrinho extends IProduto {
 }
 
 export const produtos: IProduto[] = [
-    { id: 1, nome: "Boneca de pano", tamanho: "24cm", tecido: "Algodão", categoria: "Bonecas", preco: 40.00, imagem: "assets/images/products/boneca24cm40.png" },
-    { id: 2, nome: "Boneca de pano", tamanho: "24cm", tecido: "Algodão", categoria: "Bonecas", preco: 40.00, imagem: "assets/images/products/boneca24cm40(2).png" },
-    { id: 3, nome: "Boneca de pano", tamanho: "36cm", tecido: "Algodão", categoria: "Bonecas", preco: 50.00, imagem: "assets/images/products/boneca36cm50.png" },
-    { id: 4, nome: "Boneca de pano", tamanho: "36cm", tecido: "Algodão", categoria: "Bonecas", preco: 50.00, imagem: "assets/images/products/boneca36cm50(2).png" },
-    { id: 5, nome: "Boneca de pano", tamanho: "46cm", tecido: "Algodão", categoria: "Bonecas", preco: 60.00, imagem: "assets/images/products/boneca46cm60.png" },
+    { id: 1, nome: "Boneca", tamanho: "24cm", tecido: "Algodão", categoria: "Bonecas", preco: 40.00, imagem: "assets/images/products/boneca24cm40.png" },
+    { id: 2, nome: "Boneca", tamanho: "24cm", tecido: "Algodão", categoria: "Bonecas", preco: 40.00, imagem: "assets/images/products/boneca24cm40(2).png" },
+    { id: 3, nome: "Boneca", tamanho: "36cm", tecido: "Algodão", categoria: "Bonecas", preco: 50.00, imagem: "assets/images/products/boneca36cm50.png" },
+    { id: 4, nome: "Boneca", tamanho: "36cm", tecido: "Algodão", categoria: "Bonecas", preco: 50.00, imagem: "assets/images/products/boneca36cm50(2).png" },
+    { id: 5, nome: "Boneca", tamanho: "46cm", tecido: "Algodão", categoria: "Bonecas", preco: 60.00, imagem: "assets/images/products/boneca46cm60.png" },
     { id: 6, nome: "Chaveirinho", tamanho: "7cm", tecido: "Algodão", categoria: "Chaveiros", preco: 5.00, imagem: "assets/images/products/chaveiro7cm5.png" },
     { id: 7, nome: "Chaveirinho", tamanho: "7cm", tecido: "Algodão", categoria: "Chaveiros", preco: 5.00, imagem: "assets/images/products/chaveiro7cm5(2).png" },
     { id: 8, nome: "Chaveirinho", tamanho: "7cm", tecido: "Algodão", categoria: "Chaveiros", preco: 5.00, imagem: "assets/images/products/chaveiro7cm5(3).png" },
