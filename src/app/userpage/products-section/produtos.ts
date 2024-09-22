60b876705223d9bbba267802163cfcc8419400e9
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2024-09-21 21:37:53 -0300

    :sparkles: [FEAT] Add descont.

--- src/app/userpage/products-section/produtos.ts
+++ src/app/userpage/products-section/produtos.ts
@@ -5,6 +5,7 @@ export interface IProduto {
     tecido: string,
     categoria: string;
     preco: number;
+    precodesconto?: number;
     imagem: string;
 }
 
@@ -13,8 +14,8 @@ export interface IProdutoCarrinho extends IProduto {
 }
 
 export const produtos: IProduto[] = [
-    { id: 1, nome: "Boneca", tamanho: "24cm", tecido: "Algodão", categoria: "Bonecas", preco: 40.00, imagem: "assets/images/products/boneca24cm40.png" },
-    { id: 2, nome: "Boneca", tamanho: "24cm", tecido: "Algodão", categoria: "Bonecas", preco: 40.00, imagem: "assets/images/products/boneca24cm40(2).png" },
+    { id: 1, nome: "Boneca", tamanho: "24cm", tecido: "Algodão", categoria: "Bonecas", preco: 40.00, precodesconto: 30.00, imagem: "assets/images/products/boneca24cm40.png" },
+    { id: 2, nome: "Boneca", tamanho: "24cm", tecido: "Algodão", categoria: "Bonecas", preco: 40.00, precodesconto: 30.00,imagem: "assets/images/products/boneca24cm40(2).png" },
     { id: 3, nome: "Boneca", tamanho: "36cm", tecido: "Algodão", categoria: "Bonecas", preco: 50.00, imagem: "assets/images/products/boneca36cm50.png" },
     { id: 4, nome: "Boneca", tamanho: "36cm", tecido: "Algodão", categoria: "Bonecas", preco: 50.00, imagem: "assets/images/products/boneca36cm50(2).png" },
     { id: 5, nome: "Boneca", tamanho: "46cm", tecido: "Algodão", categoria: "Bonecas", preco: 60.00, imagem: "assets/images/products/boneca46cm60.png" },
