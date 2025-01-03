2dfd8f0b90a12671324e26f88380475591171174
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2025-01-03 11:40:25 -0300

    :sparkles: [FEAT] Add More Products.

--- src/app/userpage/products-section/produtos.ts
+++ src/app/userpage/products-section/produtos.ts
@@ -27,5 +27,11 @@ export const produtos: IProduto[] = [
     { id: 11, nome: "Chaveirinho", tamanho: "13cm", tecido: "Algodão", categoria: "Chaveiros", preco: 10.00, imagem: "assets/images/products/chaveiro13cm10.png" },
     { id: 12, nome: "Chaveirinho", tamanho: "13cm", tecido: "Algodão", categoria: "Chaveiros", preco: 10.00, imagem: "assets/images/products/chaveiro13cm10(2).png" },
     { id: 13, nome: "Chaveirinho", tamanho: "13cm", tecido: "Algodão", categoria: "Chaveiros", preco: 10.00, imagem: "assets/images/products/chaveiro13cm10(3).png" },
-
-]
+    { id: 14, nome: "Boneca Bailarina", tamanho: "14cm", tecido: "Feltro", categoria: "Bonecas", preco: 15.00, imagem: "assets/images/products/boneca-bailarina-14cm-15.png" },
+    { id: 15, nome: "Boneca Bailarina", tamanho: "18cm", tecido: "Feltro", categoria: "Bonecas", preco: 15.00, imagem: "assets/images/products/boneca-bailarina-18cm-15.png" },
+    { id: 16, nome: "Boneca Juliana", tamanho: "42cm", tecido: "Feltro", categoria: "Bonecas", preco: 70.00, imagem: "assets/images/products/boneca-juliana-42cm-70.png" },
+    { id: 17, nome: "Boneca Princesa", tamanho: "33cm", tecido: "Feltro", categoria: "Bonecas", preco: 35.00, imagem: "assets/images/products/boneca-princesa-33cm-35.png" },
+    { id: 18, nome: "Boneca Unicórnio", tamanho: "35cm", tecido: "Feltro", categoria: "Bonecas", preco: 35.00, imagem: "assets/images/products/boneca-unicornio-35cm-35.png" },
+    { id: 19, nome: "Boneco Luffy", tamanho: "33cm", tecido: "Feltro", categoria: "Bonecos", preco: 35.00, imagem: "assets/images/products/boneco-luffy-33cm.png" },
+    { id: 20, nome: "Boneco Miguel", tamanho: "42cm", tecido: "Feltro", categoria: "Bonecos", preco: 70.00, imagem: "assets/images/products/boneco-miguel-42cm-70.png" }
+];
